aa14760d7d1602a37ab0552f0ba75354c0a9de2c
James Springall <james_springall@hotmail.com>
2020-07-30 21:02:12 +1000

    UPDATED | Moved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+declare var require: any;
 
+import { Component } from "@angular/core";
+import ExportingModule from "highcharts/modules/exporting";
+import SunsetTheme from "highcharts/themes/sunset.src.js";
+import * as Highcharts from "highcharts";
+
+// The modules will work for all charts.
+ExportingModule(Highcharts);
+SunsetTheme(Highcharts);
+
+Highcharts.setOptions({
+  title: {
+    style: {
+      color: "tomato",
+    },
+  },
+  legend: {
+    enabled: false,
+  },
+});
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = "Tour of Heroes";
 }
--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,24 +1,4 @@
-declare var require: any;
-
 import { Component } from "@angular/core";
-import ExportingModule from "highcharts/modules/exporting";
-import SunsetTheme from "highcharts/themes/sunset.src.js";
-import * as Highcharts from "highcharts";
-
-// The modules will work for all charts.
-ExportingModule(Highcharts);
-SunsetTheme(Highcharts);
-
-Highcharts.setOptions({
-  title: {
-    style: {
-      color: "tomato",
-    },
-  },
-  legend: {
-    enabled: false,
-  },
-});
 
 @Component({
   selector: "app-charts",
